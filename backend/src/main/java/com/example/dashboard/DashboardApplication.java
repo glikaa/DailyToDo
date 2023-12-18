17273fe2d9ece917ef350ac2a1f7a96ee404c909
Glika <glika.koutsianou@gmail.com>
2023-12-18 14:56:30 +0100

    excluded database for now

--- backend/src/main/java/com/example/dashboard/DashboardApplication.java
+++ backend/src/main/java/com/example/dashboard/DashboardApplication.java
@@ -1,13 +1,20 @@
 package com.example.dashboard;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
+@RestController
 public class DashboardApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DashboardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+      SpringApplication.run(DashboardApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 }
