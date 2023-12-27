f6c0df69608e9d03b648904512d6b488d108b937
Glika <glika.koutsianou@gmail.com>
2023-12-27 17:32:56 +0100

    logic in db needed, start of first interface

--- backend/src/main/java/com/example/tasks/repository/TaskRepository.java
+++ backend/src/main/java/com/example/tasks/repository/TaskRepository.java
@@ -1,7 +1,14 @@
 package com.example.tasks.repository;
 import com.example.tasks.classes.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+
+//call functions from db here
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Procedure(name = "AddNewTask")
+    void addNewTask(@Param("TaskName") String taskName, @Param("DueDate") Date dueDate);
 
 }
