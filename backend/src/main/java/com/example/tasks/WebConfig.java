f1a919ba5a0e6aa62a9907bd00775d2126ad8b3c
Glika <glika.koutsianou@gmail.com>
2024-01-07 11:42:28 +0100

    react app template

---
+++ backend/src/main/java/com/example/tasks/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.tasks;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") //all endpoint
+                .allowedOrigins("http://localhost:3000") // React app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
